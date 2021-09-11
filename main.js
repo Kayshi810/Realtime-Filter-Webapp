91e896d642ddec15bc6ae0a06873f69fc20f45a7
Kayshi810 <78273109+Kayshi810@users.noreply.github.com>
2021-09-11 10:38:27 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -7,11 +7,31 @@ function setup()
 {
     canvas=createCanvas(300,300);
     canvas.center();
+    video=createCapture(VIDEO);
+    video.size(300,300);
+    video.hide();
+    PoseNet=ml5.poseNet(video, modelLoaded);
+    PoseNet.on('pose', gotposes);
+}
+
+function modelLoaded()
+{
+    console.log("PoseNet is Initialized");
+}
+
+function gotposes(results)
+{
+    if(results.length > 0)
+    {
+        console.log(results);
+        console.log("nose X = "+results[0].pose.nose.x);
+        console.log("nose Y = "+results[0].pose.nose.y);
+    }
 }
 
 function draw()
 {
-
+image(video, 0, 0, 300, 300);
 }
 
 function takeSnapshot()
