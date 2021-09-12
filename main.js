3d5d45e34ae5e1fc646dd71a80251128daeb3225
Kayshi810 <78273109+Kayshi810@users.noreply.github.com>
2021-09-12 12:52:18 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
+noseX=0;
+noseY=0;
+
 function preload()
 {
-
+Lipstick_img= loadImage("https://i.postimg.cc/PxFvYgkv/l1.png");
 }
 
 function setup()
@@ -24,17 +27,20 @@ function gotposes(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose X = "+results[0].pose.nose.x);
-        console.log("nose Y = "+results[0].pose.nose.y);
+        noseX=results[0].pose.nose.x-20;
+        noseY=results[0].pose.nose.y+15;
+        console.log("nose X = "+noseX);
+        console.log("nose Y = "+noseY);
     }
 }
 
 function draw()
 {
 image(video, 0, 0, 300, 300);
+image(Lipstick_img, noseX, noseY, 50, 20);
 }
 
-function takeSnapshot()
+function take_snapshot()
 {
 save("lipstick pic.png");
 }
